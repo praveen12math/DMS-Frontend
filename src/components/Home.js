b73127610919919c1c66965acf9989b965b598bf
praveen12math <praveen12math@github.com>
2020-12-26 00:13:31 +0530

    1 copy

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,30 +1,39 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import Aos  from "aos"
+import "aos/dist/aos.css"
 import './Home.css'
 
 export default function Home() {
+    useEffect(()=>{
+      Aos.init({duration: 2000})
+    }, [])
+        
+
     return (
         <div>
-          <header id="header" className="header myBg">
+          <header className="header myBg">
         <div className="header-content">
             <div className="container">
                 <div className="row">
                     <div className="col-lg-6">
-                        <div className="text-container">
+                        <div className="text-container" data-aos="fade-right"  data-aos-delay="600">
                             <h1><span className="turquoise">Department management System</span></h1>
-                            <p className="p-large"></p>
-                            <button className="btn-primary" href="#services">Get Started</button>
                         </div>
+                        
+                <button className="btn btn-outline-primary fixed-bottom" href="#services" style={{width:"65%", left:"6%"}}>Get Started</button>
                     </div> 
                     <div className="col-lg-6">
                         <div className="image-container">
-                            <img className="img-fluid" src="header-teamwork.svg" alt="alternative"/>
+                            <img data-aos="fade-left" className="img-fluid" src="header-teamwork.svg" alt="alternative"/>
                         </div> 
                     </div> 
-                </div> 
+                </div>
+
+               
+
             </div> 
         </div> 
     </header>
-
 
         </div>
     )
