ce96f08774e43c50f4de6f5a1b026c308dee4435
ABHI <abhisahu12345@gmail.com>
2021-01-21 22:58:10 +0530

    Created leave component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ import Hod from './components/Hod';
 import Attendance from './components/Attendance'
 import Notice from './components/Notice'
 import Account from './components/Account';
+<<<<<<< HEAD
 import PageNotFound from './components/PageNotFound';
+=======
+import studentLeave from "./components/studentLeave"
+>>>>>>> 5639a59... Created Leave component
 
 export default function App() {
   return (
@@ -23,7 +27,12 @@ export default function App() {
     <Route exact path='/attendance' component={Attendance}/>
     <Route exact path='/notice' component={Notice} />
     <Route exact path='/account' component={Account} />
+<<<<<<< HEAD
     <Route component={PageNotFound} />
+=======
+    <Route exact path='/studentLeave' component={studentLeave} />
+
+>>>>>>> 5639a59... Created Leave component
 
     </Switch>  
     </Router>
