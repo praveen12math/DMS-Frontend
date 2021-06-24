f50b2531c7f1061df805516b2be9b7898f897c54
Praveen Yadav <42298393+praveen12math@users.noreply.github.com>
2021-06-24 17:42:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -34,7 +34,6 @@ export default function App() {
     <Route exact path='/account' component={Account} />
     <Route exact path='/studentLeave' component={studentLeave} />
     <Route exact path='/books' component={Books} />
-    <Route exact path="*" component={PageNotFound} />
     <Route exact path='/responseLeave' component={LeaveResponseTeacher} />
     <Route exact path="*" component={PageNotFound} />    
 
